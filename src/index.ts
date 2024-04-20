0f35ad2c84f1bdf09ee09c29bef7a3a171b8479e
Siah <siah@goodwork.sg>
2024-04-20 10:12:47 +0900

    feat: hacking

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,100 @@
-/**
- * Welcome to Cloudflare Workers!
- *
- * This is a template for a Scheduled Worker: a Worker that can run on a
- * configurable interval:
- * https://developers.cloudflare.com/workers/platform/triggers/cron-triggers/
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import Anthropic from '@anthropic-ai/sdk';
+
+function base64Encode(buf: ArrayBuffer) {
+	let string = '';
+	new Uint8Array(buf).forEach((byte) => {
+		string += String.fromCharCode(byte);
+	});
+	return btoa(string);
+}
+
+function base64Decode(string: string) {
+	string = atob(string);
+	const length = string.length,
+		buf = new ArrayBuffer(length),
+		bufView = new Uint8Array(buf);
+	for (var i = 0; i < length; i++) {
+		bufView[i] = string.charCodeAt(i);
+	}
+	return buf;
+}
 
 export default {
-	// The scheduled handler is invoked at the interval set in our wrangler.toml's
-	// [[triggers]] configuration.
-	async scheduled(event: ScheduledEvent, env: Env, ctx: ExecutionContext): Promise<void> {
-		// A Cron Trigger can make requests to other endpoints on the Internet,
-		// publish to a Queue, query a D1 Database, and much more.
-		//
-		// We'll keep it simple and make an API call to a Cloudflare API:
-		let resp = await fetch('https://api.cloudflare.com/client/v4/ips');
-		let wasSuccessful = resp.ok ? 'success' : 'fail';
+	async fetch(req: Request, env: Env) {
+		const params = new URL(req.url).searchParams;
 
-		// You could store this result in KV, write to a D1 Database, or publish to a Queue.
-		// In this template, we'll just log the result:
-		console.log(`trigger fired at ${event.cron}: ${wasSuccessful}`);
+		const mode = params.get('mode');
+		const imgKey = params.get('imgKey');
+		const cam = params.get('cam');
+
+		if (mode !== null && imgKey !== null) {
+			const object = await env.R2.get(imgKey);
+
+			if (object === null) {
+				return new Response('Object Not Found', { status: 404 });
+			}
+
+			const headers = new Headers();
+			object.writeHttpMetadata(headers);
+
+			return new Response(object.body);
+		} else if (cam !== null) {
+			const { results } = await env.DB.prepare('SELECT * FROM Results WHERE camera = ?').bind(cam).all();
+			return Response.json(results);
+		} else {
+			return new Response('Object Not Found', { status: 404 });
+		}
+	},
+
+	async scheduled(event: ScheduledEvent, env: Env) {
+		const anthropic = new Anthropic({
+			apiKey: env.ANTHROPIC_API_KEY,
+			baseURL: 'https://gateway.ai.cloudflare.com/v1/424353e86dca02b3e534d0db7eebb15d/bision-vision/anthropic',
+		});
+
+		const rand = Math.floor(Math.random() * 10000);
+		const key = new Date().toISOString();
+		//const url = 'https://datamall.lta.gov.sg/trafficsmart/images/4703_0553_20240420055407_4ff1d0.jpg';
+		const url = 'https://p5.fgies.com/bucket-klp/KLP-03.jpg';
+		//const url = 'https://qs.fgies.com/bucket-elite/ELITE-01.jpg';
+		let upstreamResponse = await fetch(`${url}?${rand}`, {
+			headers: {
+				Referer: 'https://www.jalanow.com/',
+			},
+		});
+
+		const text = base64Encode(await upstreamResponse.arrayBuffer());
+		const binary = base64Decode(text);
+
+		const put = await env.R2.put(key, binary);
+
+		const msg = await anthropic.messages.create({
+			model: 'claude-3-opus-20240229',
+			max_tokens: 1000,
+			temperature: 0,
+			messages: [
+				{
+					role: 'user',
+					content: [
+						{
+							type: 'text',
+							text: 'out of a score between 0 to 10, 10 being very crowded, how crowded is the road in this image? return just the score as one integer.',
+						},
+						{
+							type: 'image',
+							source: {
+								type: 'base64',
+								media_type: 'image/jpeg',
+								data: text,
+							},
+						},
+					],
+				},
+			],
+		});
+
+		const cam = 'KL-PUTRAJAYA NEAR SALAK SELATAN';
+		const score = isNaN(Number(msg.content?.[0]?.text)) ? -1 : Number(msg.content?.[0]?.text);
+		await env.DB.prepare(`INSERT INTO Results (camera, score, img_url) VALUES ('${cam}', ${score}, '${key}.jpg')`).run();
 	},
 };
